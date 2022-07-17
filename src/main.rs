5f300812e163cbb235cb56501687cad61466fa0a
314ShadePi <erikkirkegaardstage@gmail.com>
2022-07-17 13:02:54 +0200

    t

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
 mod games;
 
 use actix_web::{App, HttpServer};
+use actix_cors::Cors;
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
+        let cors = Cors::default()
+        .allow_any_origin()
+        .allowed_methods(vec!["GET"]);
+
         App::new()
+        .wrap(cors)
         .configure(crate::games::api::games_cfg)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
